8d3c5c272fc9b3420b381bacf8efd97dd8e7f8c4
Tiago Santos <tiagoserafimsousasantos@hotmail.com>
2024-01-09 17:32:25 +0000

    Company Cover component with badge and motivation text

---
+++ app/components/CompanyCover/CompanyCover.tsx
@@ -0,0 +1,17 @@
+import "./CompanyCover.css";
+
+const CompanyCover = () => (
+    <div className="image-container">
+        <div className="image" />
+        <div className="text-inside-img">
+            <div className="badge" />
+            <span>
+                It is a long established fact that a reader will be
+                distracted by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum
+            </span>
+        </div>
+    </div>
+);
+
+export default CompanyCover;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Navbar from "./components/Navbar/Navbar";
+import CompanyCover from "./components/CompanyCover/CompanyCover";
 
 const App = () => (
     <div>
         <Navbar />
+        <CompanyCover />
         <h1>My App</h1>
         <p>Some content</p>
     </div>
