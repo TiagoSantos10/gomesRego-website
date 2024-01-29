78d5d538c7e2d75a01f2b101efb09816f74e16f8
Tiago Santos <56252738+TiagoSantos10@users.noreply.github.com>
2024-01-29 17:15:36 +0000

    [SGR-37] - Slide Right and left animations for the team page and slide top in the social media icons (#8)

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect } from "react";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Montserrat } from "next/font/google";
@@ -6,105 +9,134 @@ import Navbar from "../components/Navbar/Navbar";
 import person from "../../public/assets/person.png";
 import "../../public/fontawesome";
 import "./team.css";
+import "../animations/animations.css";
 
 const montserrat = Montserrat({ subsets: ["latin"] }); 
 
-const Team = () => (
-    <div>
-        <Navbar />
-        <div className="team-container">
-            <div className="associate-one-container">
-                <div className="associate-one-left">
-                    <Image src={person} alt="Dulce Gomes" width={300} height={300} />
-                </div>
-                <div className="associate-one-right">
-                    <h1 className={montserrat.className}>Dulce<br />Gomes</h1>
-                    <div>
-                        descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+const Team = () => {
+    useEffect(() => {
+        const animatedElementFromRight = document.getElementsByClassName("second-associate");
+        const animatedElementFromLeft = document.getElementsByClassName("associate-left");
+
+        const observerLeft = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("slide-in-left");
+                    observer.unobserve(entry.target);
+                }
+            });
+        });
+
+        const observerRight = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("slide-in-right");
+                    observer.unobserve(entry.target);
+                }
+            });
+        });
+
+        Array.from(animatedElementFromRight).forEach(el => observerRight.observe(el));
+        Array.from(animatedElementFromLeft).forEach(el => observerLeft.observe(el));
+    }, []);
+
+    return (
+        <div>
+            <Navbar />
+            <div className="team-container">
+                <div className="associate-one-container">
+                    <div className="associate-one-left slide-in-left">
+                        <Image src={person} alt="Dulce Gomes" width={300} height={300} />
                     </div>
-                    <div className="associate-one-socials">
-                        <a href={"mailto:tiagoserafimsousasantos@gmail.com"} target="_blank">
-                            <FontAwesomeIcon icon={["fas", "envelope-square"]} size="2xl" color="var(--gras-background-blue)" />
-                        </a>
-                        <a href="https://www.facebook.com/" target="_blank">
-                            <FontAwesomeIcon icon={["fab", "facebook-square"]} size="2xl" color="var(--gras-background-blue)" />
-                        </a>
-                        <a href="https://pt.linkedin.com/" target="_blank">
-                            <FontAwesomeIcon icon={["fab", "linkedin"]} size="2xl" color="var(--gras-background-blue)" />
-                        </a>
+                    <div className="associate-one-right slide-in-left">
+                        <h1 className={montserrat.className}>Dulce<br />Gomes</h1>
+                        <div>
+                            descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        </div>
+                        <div className="associate-one-socials">
+                            <a href={"mailto:tiagoserafimsousasantos@gmail.com"} target="_blank">
+                                <FontAwesomeIcon icon={["fas", "envelope-square"]} size="2xl" color="var(--gras-background-blue)" />
+                            </a>
+                            <a href="https://www.facebook.com/" target="_blank">
+                                <FontAwesomeIcon icon={["fab", "facebook-square"]} size="2xl" color="var(--gras-background-blue)" />
+                            </a>
+                            <a href="https://pt.linkedin.com/" target="_blank">
+                                <FontAwesomeIcon icon={["fab", "linkedin"]} size="2xl" color="var(--gras-background-blue)" />
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="associate-two-container">
-                <div className="associate-two-left">
-                    <h1 className={montserrat.className}>Dulce<br />Gomes</h1>
-                    <div className="associate-two-description">
-                        descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                <div className="associate-two-container">
+                    <div className="associate-two-left second-associate">
+                        <h1 className={montserrat.className}>Dulce<br />Gomes</h1>
+                        <div className="associate-two-description">
+                            descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        </div>
+                        <div className="associate-two-socials">
+                            <a href={"mailto:tiagoserafimsousasantos@gmail.com"} target="_blank">
+                                <FontAwesomeIcon icon={["fas", "envelope-square"]} size="2xl" color="var(--gras-beige)" />
+                            </a>
+                            <a href="https://www.facebook.com/" target="_blank">
+                                <FontAwesomeIcon icon={["fab", "facebook-square"]} size="2xl" color="var(--gras-beige)" />
+                            </a>
+                            <a href="https://pt.linkedin.com/" target="_blank">
+                                <FontAwesomeIcon icon={["fab", "linkedin"]} size="2xl" color="var(--gras-beige)" />
+                            </a>
+                        </div>
                     </div>
-                    <div className="associate-two-socials">
-                        <a href={"mailto:tiagoserafimsousasantos@gmail.com"} target="_blank">
-                            <FontAwesomeIcon icon={["fas", "envelope-square"]} size="2xl" color="var(--gras-beige)" />
-                        </a>
-                        <a href="https://www.facebook.com/" target="_blank">
-                            <FontAwesomeIcon icon={["fab", "facebook-square"]} size="2xl" color="var(--gras-beige)" />
-                        </a>
-                        <a href="https://pt.linkedin.com/" target="_blank">
-                            <FontAwesomeIcon icon={["fab", "linkedin"]} size="2xl" color="var(--gras-beige)" />
-                        </a>
+                    <div className="associate-two-right second-associate">
+                        <Image src={person} alt="Dulce Gomes" width={300} height={300} />
                     </div>
                 </div>
-                <div className="associate-two-right">
-                    <Image src={person} alt="Dulce Gomes" width={300} height={300} />
-                </div>
-            </div>
-            <div className="associate-one-container">
-                <div className="associate-one-left">
-                    <Image src={person} alt="Dulce Gomes" width={300} height={300} />
-                </div>
-                <div className="associate-one-right">
-                    <h1 className={montserrat.className}>Dulce<br />Gomes</h1>
-                    <div>
-                        descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                <div className="associate-one-container">
+                    <div className="associate-one-left associate-left">
+                        <Image src={person} alt="Dulce Gomes" width={300} height={300} />
                     </div>
-                    <div className="associate-one-socials">
-                        <a href={"mailto:tiagoserafimsousasantos@gmail.com"} target="_blank">
-                            <FontAwesomeIcon icon={["fas", "envelope-square"]} size="2xl" color="var(--gras-background-blue)" />
-                        </a>
-                        <a href="https://www.facebook.com/" target="_blank">
-                            <FontAwesomeIcon icon={["fab", "facebook-square"]} size="2xl" color="var(--gras-background-blue)" />
-                        </a>
-                        <a href="https://pt.linkedin.com/" target="_blank">
-                            <FontAwesomeIcon icon={["fab", "linkedin"]} size="2xl" color="var(--gras-background-blue)" />
-                        </a>
+                    <div className="associate-one-right associate-left">
+                        <h1 className={montserrat.className}>Dulce<br />Gomes</h1>
+                        <div>
+                            descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            descrição sobre a empresa - Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        </div>
+                        <div className="associate-one-socials">
+                            <a href={"mailto:tiagoserafimsousasantos@gmail.com"} target="_blank">
+                                <FontAwesomeIcon icon={["fas", "envelope-square"]} size="2xl" color="var(--gras-background-blue)" />
+                            </a>
+                            <a href="https://www.facebook.com/" target="_blank">
+                                <FontAwesomeIcon icon={["fab", "facebook-square"]} size="2xl" color="var(--gras-background-blue)" />
+                            </a>
+                            <a href="https://pt.linkedin.com/" target="_blank">
+                                <FontAwesomeIcon icon={["fab", "linkedin"]} size="2xl" color="var(--gras-background-blue)" />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Team;
