c67b2836b392404f110fdd9cfaa658757559b30d
Tiago Santos <tiago@Tiagos-Air.lan>
2024-03-15 16:09:34 +0000

    sectors container

--- app/components/ServicesPage/SectorsContainer/SectorsContainer.tsx
+++ app/components/ServicesPage/SectorsContainer/SectorsContainer.tsx
@@ -1,43 +1,94 @@
-import { Montserrat } from "next/font/google";
+import { Almarai } from "next/font/google";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "../../../../public/fontawesome";
 import "./SectorsContainer.css";
 
-const montserrat = Montserrat({ subsets: ["latin"] });
+const almarai = Almarai({
+    subsets: ["arabic"],
+    weight: "400"
+});
 
 const SectorsContainer = () => (
     <div className="sectors-container">
-        <h1 className={montserrat.className}>Setores de Atividade</h1>
-        <div className="sectors-divider">
-            <div className="public-sector-left">
-                <h2 className={montserrat.className}>Setor<br />Público</h2>
-                <ul>
-                    <li>
-                        Entidades Reguladoras;
-                    </li>
-                    <li>
-                        Institutos Públicos, Entidades Públicas Empresariais;
-                    </li>
-                    <li>
-                        Entidades Empresariais Municipais, Municípios,
-                        Serviços Municipalizados e Associações de Municípios.
-                    </li>
-                </ul>
+        <h1 className={almarai.className}>Setores de Atividade</h1>
+        <div className="public-sector">
+            <div className="public-left-side">
+                <h3 className={almarai.className}>Setor Público</h3>
+                <div className="sector-item">
+                    <FontAwesomeIcon
+                        icon={["far", "circle-check"]}
+                        size="1x"
+                        color="var(--gras-purple)"
+                        className="circle-icon"
+                    />
+                    <div>Entidades Reguladoras</div>
+                </div>
+                <div className="sector-item">
+                    <FontAwesomeIcon
+                        icon={["far", "circle-check"]}
+                        size="1x"
+                        color="var(--gras-purple)"
+                        className="circle-icon"
+                    />
+                    <div>Institutos Públicos, Entidades Públicas Empresariais</div>
+                </div>
+                <div className="sector-item">
+                    <FontAwesomeIcon
+                        icon={["far", "circle-check"]}
+                        size="1x"
+                        color="var(--gras-purple)"
+                        className="circle-icon"
+                    />
+                    <div>Entidades Empresariais Municipais, Municípios, Serviços Municipalizados e  Associações de Municípios</div>
+                </div>
             </div>
-            <div className="private-sector-right">
-                <h2 className={montserrat.className}>Setor<br />Privado</h2>
-                <ul>
-                    <li>
-                        Agricultura, Pescas, Aquacultura, Avicultura, Pecuária, Vitivinicultura, Agro-indústria;
-                    </li>
-                    <li>
-                        Construção Civil e Obras Públicas;
-                    </li>
-                    <li>
-                        Indústria Transformadora, Comércio, Prestação de Serviços;
-                    </li>
-                    <li>
-                        Promoção Imobiliária, Gestão de Participações Sociais.
-                    </li>
-                </ul>
+            <div className="public-right-side">
+                <div className="public-img" />
+            </div>
+        </div>
+        <div className="private-sector">
+            <div className="private-left-side">
+                <div className="private-img" />
+            </div>
+            <div className="private-right-side">
+                <h3 className={almarai.className}>Setor Privado</h3>
+                <div className="sector-item">
+                    <FontAwesomeIcon
+                        icon={["far", "circle-check"]}
+                        size="1x"
+                        color="var(--gras-purple)"
+                        className="circle-icon"
+                    />
+                    <div>Agricultura, Pescas, Aquacultura, Avicultura, Pecuária, Vitivinicultura, Agro-indústria</div>
+                </div>
+                <div className="sector-item">
+                    <FontAwesomeIcon
+                        icon={["far", "circle-check"]}
+                        size="1x"
+                        color="var(--gras-purple)"
+                        className="circle-icon"
+                    />
+                    <div>Construção Civil e Obras Públicas</div>
+                </div>
+                <div className="sector-item">
+                    <FontAwesomeIcon
+                        icon={["far", "circle-check"]}
+                        size="1x"
+                        color="var(--gras-purple)"
+                        className="circle-icon"
+                    />
+                    <div>Indústria Transformadora, Comércio e Prestação de Serviços</div>
+                </div>
+                <div className="sector-item">
+                    <FontAwesomeIcon
+                        icon={["far", "circle-check"]}
+                        size="1x"
+                        color="var(--gras-purple)"
+                        className="circle-icon"
+                    />
+                    <div>Promoção Imobiliária e Gestão de Participações Sociais</div>
+                </div>
             </div>
         </div>
     </div>
