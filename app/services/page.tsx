7e3168cdf21a1a34d424daf27c44ac4e9878f455
Tiago Santos <56252738+TiagoSantos10@users.noreply.github.com>
2024-03-21 11:07:31 +0000

    SGR-46 - Services Section & Improve Navbar (#17)

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar/Navbar";
 import SectorsContainer from "../components/ServicesPage/SectorsContainer/SectorsContainer";
+import ServicesContainer from "../components/ServicesPage/ServicesContainer/ServicesContainer";
 
 const Services = () => (
     <div>
         <Navbar />
         <SectorsContainer />
+        <ServicesContainer />
     </div>
 );
 
