e641a4ef2b97d49ab88b58af58fb6c4bbd59006c
Tiago Santos <56252738+TiagoSantos10@users.noreply.github.com>
2024-03-21 11:36:56 +0000

    SGR-11 - Footer #18

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,7 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Footer.css";
 
 const Footer = () => (
-    <div id="footer">Footer</div>
+    <footer id="footer">
+        <div className="footer-socials">
+            <a href="https://pt.linkedin.com/" target="_blank">
+                <FontAwesomeIcon icon={["fab", "linkedin-in"]} size="lg" color="var(--gras-background-blue)" />
+            </a>
+            <a href={"mailto:tiagoserafimsousasantos@gmail.com"} target="_blank">
+                <FontAwesomeIcon icon={["fas", "envelope"]} size="lg" color="var(--gras-background-blue)" />
+            </a>
+            
+        </div>
+        <div className="footer-text">
+            © 2024 Gomes Rego e Associados. All rights reserved.
+        </div>
+    </footer>
 );
 
 export default Footer;
