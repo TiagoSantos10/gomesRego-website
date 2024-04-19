7c9eea423d950f3239f3450bed717a521aa401d8
Tiago Santos <tiago@Tiagos-Air.lan>
2024-04-19 15:44:41 +0100

    fix: add documentation

--- services/apis/contentful/contentful.ts
+++ services/apis/contentful/contentful.ts
@@ -1,3 +1,7 @@
+/**
+ * Create the client to connect to Contentful
+ * @returns {Object} client
+ */
 const createClient = () => {
     const contentful = require("contentful");
     const client = contentful.createClient({
@@ -8,18 +12,30 @@ const createClient = () => {
     return client;
 };
 
+/**
+ * Function to get the news items from Contentful
+ * @returns {Array} Array containing the news items
+ */
 export const getNews = async () => {
     const client = createClient();
     const response = await client.getEntries({ content_type: "news" });
     return response.items;
 };
 
+/**
+ * Function to get the news categories from Contentful
+ * @returns {Array} Array containing the news categories
+ */
 export const getCategories = async () => {
     const client = createClient();
     const response = await client.getEntries({ content_type: "newsCategory" });
     return response.items;
 };
 
+/**
+ * Function to get the news page content from Contentful
+ * @returns {Object} Object containing the news page content
+ */
 export const getNewsPage = async () => {
     const client = createClient();
     const response = await client.getEntries({ content_type: "newsPage" });
