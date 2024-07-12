10ac6603a459fe9737bc3b07dd56a15169394872
Tiago Santos <tiago@tiagos-air.lan>
2024-07-12 15:17:21 +0100

    partners section in mobile sizes

--- app/components/HomePage/PartnersSection/PartnerCard/PartnerCard.tsx
+++ app/components/HomePage/PartnersSection/PartnerCard/PartnerCard.tsx
@@ -1,15 +1,32 @@
 import Link from "next/link";
+import { Almarai } from "next/font/google";
 import { PartnersType } from "@/app/utils/types/types";
 import styles from "./PartnerCard.module.css";
+
+const almarai = Almarai({
+    subsets: ["arabic"],
+    weight: "400"
+});
 
 const PartnerCard = ({ partner }: { partner: PartnersType }) => {
     const { name, image } = partner.fields;
 
     return (
-        <Link className={styles.partnerCard} href="/team">
-            <img src={image.fields.file.url} alt={name} className={styles.partnerImage} />
-            <div className={styles.partnerName}>{name}</div>
-        </Link>
+        <>
+            <div className={styles.desktopCard}>
+                <Link href="/team">
+                    <img src={image.fields.file.url} alt={name} className={styles.partnerImage} />
+                    <div className={styles.partnerName}>{name}</div>
+                </Link>
+            </div>
+            <Link href="/team" className={styles.mobileCard}>
+                <img src={image.fields.file.url} alt={name} className={styles.partnerImageMobile} />
+                <div className={styles.partnerNameMobileSection}>
+                    <h3 className={`${styles.nameMobile} ${almarai.className}`}>{name}</h3>
+                    <p className={styles.roleMobile}>Senior Partner</p>
+                </div>
+            </Link>
+        </>
     );
 };
 
