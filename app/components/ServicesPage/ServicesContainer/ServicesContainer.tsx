2127d580f29f2c4d77b2fc17c3bab1dddb595609
Tiago Santos <56252738+TiagoSantos10@users.noreply.github.com>
2025-06-13 17:31:45 +0100

    Services Section inside Services & Values page (#55)

--- app/components/ServicesPage/ServicesContainer/ServicesContainer.tsx
+++ app/components/ServicesPage/ServicesContainer/ServicesContainer.tsx
@@ -1,20 +1,16 @@
 "use client";
 
-import { Almarai } from "next/font/google";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, A11y } from "swiper/modules";
 import { useEffect, useState } from "react";
-import { ServicesType } from "@/app/utils/types/types";
+import { ServicesSectionType } from "@/app/utils/types/types";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/manipulation";
 import styles from "./ServicesContainer.module.css";
-
-const almarai = Almarai({
-    subsets: ["arabic"],
-    weight: "400"
-});
+import SectionHeader from "../../Common/SectionHeader/SectionHeader";
+import SectionDescription from "../../Common/SectionDescription/SectionDescription";
 
 type SwiperSlideElementType = {
     text: string;
@@ -33,35 +29,41 @@ const SwiperSlideElement = ({ text, icon }: SwiperSlideElementType) => {
 };
 
 type ServicesContainerProps = {
-    services: ServicesType[];
-    sectionHeader: string;
+    servicesSectionData: ServicesSectionType;
 };
 
 const ServicesContainer: React.FC<ServicesContainerProps> = ({
-    services,
-    sectionHeader
+    servicesSectionData
 }) => {
     const [slidesPerView, setSlidesPerView] = useState(5);
 
     useEffect(() => {
         const windowSize = window.innerWidth;
-        const slidesNumber = windowSize < 630 ? 1 : (windowSize < 792 ? 2 : 5);   
-        setSlidesPerView(slidesNumber); 
+        const slidesNumber = windowSize < 630 ? 1 : (windowSize < 792 ? 2 : 5);
+        setSlidesPerView(slidesNumber);
     }, []);
+
+    const {
+        title,
+        description,
+        servicesList
+    } = servicesSectionData.fields;
 
     return (
         <section className={styles.servicesContainer}>
-            <h1 className={`${almarai.className} section-title`}>{sectionHeader}</h1>
+            <SectionHeader title={title} />
+            <SectionDescription description={description} />
             <Swiper
                 modules={[Navigation, Pagination, A11y]}
+                spaceBetween={20}
                 slidesPerView={slidesPerView}
                 slidesPerGroup={slidesPerView}
                 navigation={true}
                 pagination={{ clickable: true }}
-                style={{ width: "100%", height: "200px"}}
+                style={{ width: "100%", height: "200px" }}
             >
                 {
-                    services.map((service, index) => (
+                    servicesList.map((service, index) => (
                         <SwiperSlide key={index} className={styles["swiper-slide"]}>
                             <SwiperSlideElement
                                 text={service.fields.name}
